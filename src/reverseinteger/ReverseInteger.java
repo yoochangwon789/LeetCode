73d462da5f456f3796f5a2cc60eb497f8c1120b1
changwon <ckddnjs554@naver.com>
2021-01-04 23:37:39 +0900

    ReverseInteger 정수 역순 출력

---
+++ src/reverseinteger/ReverseInteger.java
@@ -0,0 +1,19 @@
+package reverseinteger;
+
+public class ReverseInteger {
+    public static void main(String[] args) {
+
+        int num;
+
+        num = 10/123;
+        System.out.println(num);
+    }
+
+    public int reverse(int x) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+}
