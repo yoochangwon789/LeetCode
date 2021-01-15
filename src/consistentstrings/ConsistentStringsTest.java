216a905ac4d68d03cc6b7593662569d5126cc3a5
changwon <ckddnjs554@naver.com>
2021-01-15 17:22:12 +0900

    Feat : 연관 문자열 찾기 연습

--- src/consistentstrings/ConsistentStringsTest.java
+++ src/consistentstrings/ConsistentStringsTest.java
@@ -1,27 +1,44 @@
 package consistentstrings;
 
+import java.util.ArrayList;
+
 public class ConsistentStringsTest {
     public static void main(String[] args) {
 
         String allowed = "ab";
-        char[] colander = new char[allowed.length()];
         String[] words = {"ad", "bd", "aaab", "baa", "badab"};
-        int[] wordsLength = new int[words.length];
+
+        ArrayList<String> arrayList = new ArrayList();
+
+        String[] arr = new String[allowed.length()];
+
+        for (int i = 0; i < allowed.length(); i++) {
+            arr[i] = String.valueOf(allowed.charAt(i));
+        }
 
         int cnt = 0;
 
-        for (int i = 0; i < wordsLength.length; i++) {
-            wordsLength[i] = words[i].length();
-        }
-
-        for (int i = 0; i < allowed.length(); i++) {
-            colander[i] = allowed.charAt(i);
-        }
-
-        for (int i = 0; i < words.length; i++) {
-            for (int j = 0; j < allowed.length(); j++) {
-                if (words[i].indexOf(allowed.charAt(j)))
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < words.length; j++) {
+                arrayList.add(words[j].replace(arr[i], ""));
             }
         }
+
+        String[] answer = new String[arrayList.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = arrayList.get(i);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < answer.length; j++) {
+                if (answer[j].indexOf(arr[i]) < 0) {
+                    arrayList.remove(j);
+                }
+            }
+        }
+
+
+        System.out.println(arrayList);
     }
 }
