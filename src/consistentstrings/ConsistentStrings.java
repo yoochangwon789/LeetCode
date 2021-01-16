3da773980c65b5517090e967e22d4a7c383f81d2
changwon <ckddnjs554@naver.com>
2021-01-16 19:42:35 +0900

    Feat : 연관 문자열 찾기 Set 방법으로 해결

--- src/consistentstrings/ConsistentStrings.java
+++ src/consistentstrings/ConsistentStrings.java
@@ -1,4 +1,36 @@
 package consistentstrings;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ConsistentStrings {
+    public static void main(String[] args) {
+        String allowed = "ab";
+        String[] words = {"ad","bd","aaab","baa","badab"};
+
+        System.out.println(countConsistentStrings(allowed, words));
+    }
+
+    public static int countConsistentStrings(String allowed, String[] words) {
+        Set<Character> set = new HashSet<>();
+
+        for (int i = 0; i < allowed.length(); i++) {
+            set.add(allowed.charAt(i));
+        }
+
+        int cnt = 0;
+
+        for (String word : words) {
+            boolean flag = true;
+            for (int j = 0; j < word.length(); j++) {
+                if (!set.contains(word.charAt(j))) {
+                    flag = false;
+                }
+            }
+            if (flag) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
 }
