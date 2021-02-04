5ae02e805e7b5fbd7af315eca988438a3f1e644e
changwon <ckddnjs554@naver.com>
2021-02-04 19:33:08 +0900

    Feat : 배열 수열식으로 더하기

---
+++ src/runningsum/RunningSum.java
@@ -0,0 +1,15 @@
+package runningsum;
+
+public class RunningSum {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] runningSum(int[] nums) {
+        int[] answer = {};
+
+
+
+        return answer;
+    }
+}
