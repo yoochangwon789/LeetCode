4c54f198c47a019fd21b6b1d233cf6e1f63fd8e6
changwon <ckddnjs554@naver.com>
2021-03-07 20:56:37 +0900

    Feat : MergeStringsAlternately 문제 해결 중

---
+++ src/mergestringsalternately/MergeStringsAlternately.java
@@ -0,0 +1,19 @@
+package mergestringsalternately;
+
+public class MergeStringsAlternately {
+    public static void main(String[] args) {
+
+    }
+
+    public static String mergeAlternately(String word1, String word2) {
+        String answer = "";
+
+        if (word1.length() == word2.length()) {
+            for (int i = 0; i < word1.length(); i++) {
+
+            }
+        }
+
+        return answer;
+    }
+}
