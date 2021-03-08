0c3b33cdddc05e4772f5f944d35481c1f50d5af3
changwon <ckddnjs554@naver.com>
2021-03-08 20:40:55 +0900

    Feat : FinalPrice 문제 해결 중

---
+++ src/finalprice/FinalPrice.java
@@ -0,0 +1,23 @@
+package finalprice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FinalPrice {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] finalPrices(int[] prices) {
+        int[] answer = {};
+
+        int cnt = 0;
+        List<Integer> integerList = new ArrayList<>();
+
+        for (int i = 0; i < prices.length; i++) {
+
+        }
+
+        return answer;
+    }
+}
