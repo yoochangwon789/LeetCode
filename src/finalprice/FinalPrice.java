323d8a9aba9c439a05dcbf5988f74eebf59d3877
changwon <ckddnjs554@naver.com>
2021-03-08 22:49:52 +0900

    Feat : FinalPrice List Collection 사용중

--- src/finalprice/FinalPrice.java
+++ src/finalprice/FinalPrice.java
@@ -5,17 +5,19 @@ import java.util.List;
 
 public class FinalPrice {
     public static void main(String[] args) {
+        int[] prices = {8,4,6,2,3};
 
+        finalPrices(prices);
+        System.out.println("test");
     }
 
     public static int[] finalPrices(int[] prices) {
         int[] answer = {};
 
-        int cnt = 0;
         List<Integer> integerList = new ArrayList<>();
 
         for (int i = 0; i < prices.length; i++) {
-
+            integerList.add(prices[i]);
         }
 
         return answer;
