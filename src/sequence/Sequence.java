77cd2569fc4ef72a6de00eff57b06de11c6d3e02
changwon <ckddnjs554@naver.com>
2021-03-10 22:53:36 +0900

    Feat : Sequence 문제 해결 중

---
+++ src/sequence/Sequence.java
@@ -0,0 +1,15 @@
+package sequence;
+
+public class Sequence {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean canMakeArithmeticProgression(int[] arr) {
+        boolean answer;
+
+
+
+        return true;
+    }
+}
