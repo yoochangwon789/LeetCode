bc5457a08a2548b47cc9aecb2e0efffc990a0b3b
changwon <ckddnjs554@naver.com>
2021-03-10 23:02:18 +0900

    Feat : Sequence 문제 Collections 를 통한 알고리즘 해결 중

--- src/sequence/Sequence.java
+++ src/sequence/Sequence.java
@@ -1,5 +1,9 @@
 package sequence;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Sequence {
     public static void main(String[] args) {
 
@@ -8,7 +12,18 @@ public class Sequence {
     public static boolean canMakeArithmeticProgression(int[] arr) {
         boolean answer;
 
+        List<Integer> integerList = new ArrayList<>();
 
+        for (int i = 0; i < arr.length; i++) {
+            integerList.add(arr[i]);
+        }
+
+        Collections.sort(integerList);
+
+        int cnt = 0;
+
+        for (int i = 1; i < integerList.size(); i++) {
+        }
 
         return true;
     }
